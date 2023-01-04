c62c9ae5ae2d36b8bdda05b4da4e528b13fac303
0x7266 <rrrr0ffff@gmail.com>
2023-01-03 22:38:17 -0300

    game logic

--- src/App.tsx
+++ src/App.tsx
@@ -8,7 +8,8 @@ type Combination = {
 function App() {
   const [cells, setCells] = useState<number[]>([0, 0, 0, 0, 0, 0, 0, 0, 0]);
   const [turn, setTurn] = useState<number>(1);
-  const [winningCombo, setWinningCombo] = useState<number[] | null>(null);
+  const [winningCombo, setWinningCombo] = useState<Combination | null>(null);
+  const [winner, setWinner] = useState<string | null>(null);
 
   const cellBorderConditions = (index: number) => {
     if (index === 0) {
@@ -56,25 +57,28 @@ function App() {
     if (cells[index] !== 0) {
       return;
     }
-    console.log(index);
+    if (winningCombo) {
+      return;
+    }
     setCells((prev) => {
-      prev[index] = turn;
-      console.log(prev);
-      return prev;
+      const updated = [...prev];
+      updated[index] = turn;
+      return updated;
     });
     setTurn(turn === 1 ? 2 : 1);
   };
 
   const checkWinner = () => {
-    let winner: string;
-    winningCombinations.map((wc) => {
-      const { indexes } = wc;
+    let winner: string | null = null;
+    for (let combination of winningCombinations) {
+      const { indexes } = combination;
       if (
         cells[indexes[0]] === 1 &&
         cells[indexes[1]] === 1 &&
         cells[indexes[2]] === 1
       ) {
         winner = "player 1";
+        setWinner(winner.toUpperCase());
       }
       if (
         cells[indexes[0]] === 2 &&
@@ -82,32 +86,64 @@ function App() {
         cells[indexes[2]] === 2
       ) {
         winner = "player 2";
+        setWinner(winner.toUpperCase());
       }
       if (winner) {
-        setWinningCombo(indexes);
+        setWinningCombo(combination);
+        break;
       }
-    });
+    }
+  };
+
+  const checkDraw = () => {
+    if (cells.every((c) => c !== 0)) {
+      alert("draw");
+    }
+  };
+
+  const reset = () => {
+    setCells([0, 0, 0, 0, 0, 0, 0, 0, 0]);
+    setTurn(1);
+    setWinningCombo(null);
   };
 
   useEffect(() => {
     checkWinner();
+    checkDraw();
   }, [cells]);
 
+  useEffect(() => {
+    if (winningCombo) {
+      alert(`${winner} venceu`);
+    }
+  }, [winningCombo]);
+
   return (
-    <div className="game h-screen bg-slate-800 flex justify-center items-center">
-      <div className="board bg-gray-800 grid grid-cols-3 shadow-2xl rounded">
+    <div className="game h-screen bg-slate-800 flex flex-col gap-10 justify-center items-center">
+      <div className="board grid grid-cols-3">
         {cells.map((cell, index) => (
           <span
-            className={`cell border border-cyan-900 text-2xl sm:text-5xl w-20 h-20 ${cellBorderConditions(
+            key={index}
+            className={`relative border border-cyan-900 text-5xl w-20 h-20 ${cellBorderConditions(
               index
-            )} flex items-center justify-center`}
+            )} ${
+              winningCombo?.indexes.includes(index)
+                ? winningCombo.orientation
+                : undefined
+            } flex items-center justify-center after:bg-slate-700 after:rounded-md`}
             onClick={() => handleClick(index)}
           >
-            {cell === 1 && "🤢"}
-            {cell === 2 && "😡"}
+            {cell === 1 && "🥓"}
+            {cell === 2 && "🥚"}
           </span>
         ))}
       </div>
+      <button
+        className="bg-slate-900 px-4 py-2 rounded text-cyan-900 font-bold relative active:top-[1px]"
+        onClick={reset}
+      >
+        RESET
+      </button>
     </div>
   );
 }
